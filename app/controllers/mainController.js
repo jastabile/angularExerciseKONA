c99b21a8c2c86915c0b23fdd263f6274415aff54
jastabile <jastabile@hotmail.com>
2017-05-20 17:40:57 -0300

    Lista de empleados y filtrado

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,6 +1,9 @@
 
 
 
-app.controller('MainController', function ($scope) {
-
+app.controller('MainController', function ($scope, $http) {
+	$http.get("https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole")
+	.then(function(response) {
+        $scope.empleados = response.data;
+    });
 });
